34af75a2242d551e2812308423aa99135778438f
kaza514 <63160591+kaza514@users.noreply.github.com>
2024-07-01 21:15:12 -0500

    Update Basics.py

--- python/Basics.py
+++ python/Basics.py
@@ -18,3 +18,26 @@ fruits = ['banana', 'apple', 'mango']
 for fruit in fruits: # Second Example
     print('Current fruit :', fruit)
 print("Good bye!")
+
+
+############
+# Example 3
+############
+# IF else in Python
+
+var1 = 100
+if var1:
+    print("1 - Got a true expression value")
+    print(var1)
+else:
+    print("1 - Got a false expression value")
+    print(var1)
+var2 = 0
+if var2:
+    print("2 - Got a true expression value")
+    print(var2)
+else:
+    print("2 - Got a false expression value")
+    print(var2)
+print("Good bye!")
+
