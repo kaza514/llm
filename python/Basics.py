8817cb3019a2e430d2bfab0f18e9cc9f8bf91e22
kaza514 <63160591+kaza514@users.noreply.github.com>
2024-07-01 21:12:49 -0500

    Create Basics.py

---
+++ python/Basics.py
@@ -0,0 +1,20 @@
+############
+# Example 1
+############
+# Get a character input from the user
+ch = input("Enter a character: ")
+# Print the ASCII value of the character
+print(ord(ch))
+
+
+############
+# Example 2
+############
+# For loop in Python
+
+for letter in 'Python': # First Example
+    print('Current Letter :', letter)
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits: # Second Example
+    print('Current fruit :', fruit)
+print("Good bye!")
