6f2f527a894247353319b64e8e4418134ecc93cf
kaza514 <63160591+kaza514@users.noreply.github.com>
2024-07-01 21:17:28 -0500

    Update Basics.py

--- python/Basics.py
+++ python/Basics.py
@@ -41,3 +41,45 @@ else:
     print(var2)
 print("Good bye!")
 
+############
+# Example 4
+############
+# Nested IF in Python
+
+var = 100
+if var < 200:
+    print("Expression value is less than 200")
+    if var == 150:
+        print("Which is 150")
+    elif var == 100:
+        print("Which is 100")
+    elif var == 50:
+        print("Which is 50")
+elif var < 50:
+    print("Expression value is less than 50")
+else:
+    print("Could not find true expression")
+print("Good bye!")
+
+
+############
+# Example 5
+############
+# Nested While
+
+i = 2
+while(i < 100):
+    j = 2
+    while(j <= (i/j)):
+        if not(i%j): 
+            break
+        j = j + 1
+        if (j > i/j) : 
+            print(i, " is prime")
+    i = i + 1
+print("Good bye kaza !")
+
+
+
+
+
