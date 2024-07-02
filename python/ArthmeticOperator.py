6a0cec92f9b5beba8d83e010a277061485d66d46
kaza514 <63160591+kaza514@users.noreply.github.com>
2024-07-01 21:05:38 -0500

    Create ArthmeticOperator.py

---
+++ python/ArthmeticOperator.py
@@ -0,0 +1,23 @@
+import math
+x = 15
+y = 4
+
+# Output: x + y = 19  "com"
+print('x + y =',x+y)
+
+# Output: x - y = 11
+print('x - y =',x-y)
+
+# Output: x * y = 60
+print('x * y =',x*y)
+
+# Output: x / y = 3.75
+print('x / y =',x/y)
+
+# Output: x // y = 3
+print('x // y =',x//y)
+
+# Output: x ** y = 50625
+print('x ** y =',x**y)
+
+print(round(12.6))
