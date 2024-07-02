8b7ebffe27155a42d09394a7d891bb3ba41e7653
kaza514 <63160591+kaza514@users.noreply.github.com>
2024-07-01 21:23:45 -0500

    Update Basics.py

--- python/Basics.py
+++ python/Basics.py
@@ -72,6 +72,17 @@ while (count < 9):
     count = count + 1
 print("Good bye kaju!")
 
+############
+# While Else
+############
+count = 0
+while count < 5:
+    print(count, " is less than 5")
+    count = count + 1
+else:
+    print(count, " is not less than 5")
+
+
 
 ############
 # Example 6
